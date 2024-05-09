b9086cffbcba388aa6e3957a037c68ee4f59d01b
Manita Paudel <paudel04manita@gmail.com>
2024-05-09 14:11:11 +0545

    Create toggle menu for the navbar links in smaller screen sizes

--- src/app/components/navbar/index.jsx
+++ src/app/components/navbar/index.jsx
@@ -1,9 +1,19 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 
 import Hamburger from "@/app/icons/hamburger";
 import "./_navbar.scss";
+import HamburgerOpen from "@/app/icons/hamburger-open";
+import Close from "@/app/icons/close";
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleToggleMenu = () => {
+    setOpenMenu((prev) => !prev);
+  };
+
   return (
     <nav className="navbar">
       <Link href="/" className="alien-home">
@@ -14,7 +24,30 @@ const Navbar = () => {
         <Link href="/">Products</Link>
         <Link href="/">Services</Link>
       </section>
-      {/* <Hamburger className="hamburger" /> */}
+      <button className="toggle-menu" onClick={handleToggleMenu}>
+        {openMenu ? (
+          <HamburgerOpen className="hamburger" />
+        ) : (
+          <Hamburger className="hamburger" />
+        )}
+      </button>
+      {openMenu ? (
+        <section className="drawer-menu">
+          <button
+            className="close-drawer-btn"
+            onClick={() => setOpenMenu(false)}
+          >
+            <Close className="close-icon" />
+          </button>
+          <section className="routes">
+            <Link href="/">Home</Link>
+            <Link href="/">Products</Link>
+            <Link href="/">Services</Link>
+          </section>
+        </section>
+      ) : (
+        ""
+      )}
     </nav>
   );
 };
